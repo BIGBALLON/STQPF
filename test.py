cc1f4ee4b67e9a9df13db31c1ddc53b658973d38
bigballon <605718199@qq.com>
2017-04-16 09:21:57 +0800

    vstack

--- test.py
+++ test.py
@@ -15,25 +15,25 @@ example_batch, label_batch, matrix_batch = tf.train.batch(
       [example, label, matrix], batch_size=BATCH_SIZE)  
 # 运行Graph
 with tf.Session() as sess:
-    coord = tf.train.Coordinator()  #创建一个协调器，管理线程
-    threads = tf.train.start_queue_runners(coord=coord)  #启动QueueRunner, 此时文件名队列已经进队。
-    for i in range(1000):
-        if coord.should_stop():
-            break
-        example, label, matrix = sess.run([example_batch,label_batch,matrix_batch])  
+	coord = tf.train.Coordinator()  #创建一个协调器，管理线程
+	threads = tf.train.start_queue_runners(coord=coord)  #启动QueueRunner, 此时文件名队列已经进队。
+	for i in range(1000):
+		if coord.should_stop():
+			break
+		example, label, matrix = sess.run([example_batch,label_batch,matrix_batch])  
 
-        x_train = np.asarray([])
+		x_train = np.asarray([])
 
-        for i in range(BATCH_SIZE):
-            tmp = str(matrix[i])
-            tmp = tmp[2:-1]
-            tmp = tmp.split()
-            if i == 0 :
-                x_train = np.reshape(np.asarray(tmp,dtype='float32'),(101,101,60))
-            else:
-                x_train =  np.concatenate((x_train,np.reshape(np.asarray(tmp,dtype='float32'),(101,101,60))), axis=0)
-        x_train = np.reshape(x_train,(BATCH_SIZE,101,101,60))
-        print(x_train.dtype)
+		for i in range(BATCH_SIZE):
+			tmp = str(matrix[i])
+			tmp = tmp[2:-1]
+			tmp = tmp.split()
+			if i == 0 :
+				x_train = np.reshape(np.asarray(tmp,dtype='float32'),(1,101,101,60))
+			else:
+				x_train = np.vstack((x_train,np.reshape(np.asarray(tmp,dtype='float32'),(1,101,101,60))))
+		# x_train = np.reshape(x_train,(BATCH_SIZE,101,101,60))
+		print(x_train.shape)
 
-    coord.request_stop()
-    coord.join(threads)
+	coord.request_stop()
+	coord.join(threads)
